d42a927d14e41cebcf13fcc8c23b34ee6897190e
Alejandro Agudelo <robertoagudeloc@gmail.com>
2025-06-19 17:34:07 -0500

    change index js file by server js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const productsRouter = require('./products');
+const cartRouter     = require('./cart');
+
+const app = express();
+app.use(express.json());
+
+//Rutas sin duplicar /api —Vercel ya las servirá bajo /api
+app.use('/products', productsRouter);
+app.use('/cart',     cartRouter);
+
+app.get('/', (_, res) => res.send('Sports Shoes API, OK'));
+
+module.exports = app;               // <-- imprescindible para Vercel
+
+// Solo levanta el puerto si estás en desarrollo local
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Local API on http://localhost:${PORT}`));
+}
